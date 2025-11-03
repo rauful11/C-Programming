62f6c2db3ce47669052546c663a56732f0af960a
MD.RAUFUL ISLAM <raufulislam820@gmail.com>
2025-11-03 11:59:09 +0600

    eligible to vot or not

---
+++ main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int age;
+    printf("Enter your age: ");
+    scanf("%d", &age);
+
+    if(age >= 18)
+    {
+        printf("%d is voter\n", age);
+    }
+    else
+    {
+        printf("%d is not voter\n", age);
+    }
+
+    return 0;
+}
