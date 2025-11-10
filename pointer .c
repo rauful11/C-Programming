e6acade4b413a05227d557bc273b6493fd89a26d
MD.RAUFUL ISLAM <raufulislam820@gmail.com>
2025-11-10 22:44:51 +0600

    Fix formatting and add address prints in pointer.c

--- pointer .c
+++ pointer .c
@@ -3,8 +3,10 @@ int main(){
     int num=50;
     int *p;
     p=&num;
-    printf("adrees of p variable %d \n",p);
-    printf("value of p variable %d",*p);
+    printf("addrees of p variable %d \n",p);
+    printf("value of p variable %d\n",*p);
+    printf(" number address %d \n",&num);
+    printf("address of pointer %d",&p);
 
     return 0;
 }
